a155c281890439e774cb39d6eb03ba2edc53454e
Reenen <reenenl@pepstores.com>
2026-02-05 14:31:36 +0000

    feat: Add newline between files in build.sh script

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This script is the Linux/macOS equivalent of build.ps1
+
+# 1. Define the list of files to be concatenated in the correct order.
+files=(
+    "./dscc.min.js"
+    "./aggregators.js"
+    "./treeBuilder.js"
+    "./layoutMetricColumn.js"
+    "./layoutMetricRow.js"
+    "./layoutMetricFirstColumn.js"
+    "./layoutMetricFirstRow.js"
+    "./headerRenderer.js"
+    "./main.js"
+)
+
+# 2. Concatenate the files into viz.js.
+# The > operator will create or overwrite viz.js with the first file.
+cat "${files[0]}" > ./viz.js
+for file in "${files[@]:1}"; do
+    echo "" >> ./viz.js # Add a newline
+    cat "$file" >> ./viz.js
+done
+
+echo "Build Complete: viz.js created in root."
